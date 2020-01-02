87bcac9a37f08973e82f0dffc73a0720b7b3677d
Raul Sanchez <raulsanchz.h@gmail.com>
2020-01-01 22:46:33 -0600

    responsive changes

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var burgerMenu = document.querySelector(".burger-menu");
 var mobileMenu = document.querySelector(".mobile-menu");
+var mobileNav = document.querySelectorAll("#mobile-nav");
 burgerMenu.addEventListener('click',function(){
     mobileMenu.classList.toggle("mobile-open")
 })
+
+console.log(mobileNav);
+mobileNav.forEach(function(item){
+    item.addEventListener('click',function(){
+        console.log("entre");
+        mobileMenu.classList.remove("mobile-open")
+    })
+})
+    
+
+
