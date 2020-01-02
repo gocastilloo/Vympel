84c80f1220b854945745faf176a16ff749dd296f
Raul Sanchez <raulsanchz.h@gmail.com>
2020-01-02 00:06:56 -0600

    full responsive

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var burgerMenu = document.querySelector(".burger-menu");
 var mobileMenu = document.querySelector(".mobile-menu");
 var mobileNav = document.querySelectorAll("#mobile-nav");
+var infoRow = document.querySelectorAll('.contact-info-row');
 burgerMenu.addEventListener('click',function(){
     mobileMenu.classList.toggle("mobile-open")
 })
 
-console.log(mobileNav);
+
 mobileNav.forEach(function(item){
     item.addEventListener('click',function(){
-        console.log("entre");
         mobileMenu.classList.remove("mobile-open")
     })
 })
-    
 
+
+ infoRow.forEach(function(item){
+     item.addEventListener('click',function(){
+         item.classList.toggle("moveToLeft")
+     })
+ })
 
